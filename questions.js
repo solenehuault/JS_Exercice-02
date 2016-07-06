6318517192b5ad43e763f3135348e6b8864dae08
erwannFace <e.duclos@fondationface.org>
2016-07-06 09:43:04 +0200

    Initialize project

---
+++ questions.js
@@ -0,0 +1,9 @@
+/**
+ * Exercice sur les chaines de caractères.
+ * Trouvez la façon de faire la plus optimal.
+ * Il peut y avoir plusieur façon de faire.
+ */
+
+/**
+ * Exercices sur les nombres et les caluls mathématiques
+ */
