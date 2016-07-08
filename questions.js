579d55dfcb223ed8d917edb3de0ccf72152745b4
solenehuault <solene.huault@gmail.com>
2016-07-08 17:07:53 +0200

    exo 2 loading

--- questions.js
+++ questions.js
@@ -7,3 +7,74 @@
 /**
  * Exercices sur les nombres et les caluls mathématiques
  */
+
+function tailleString(str) {
+	return (str.length);
+};
+
+function remplaceCar(str, element, remplace) {
+	return (str.replace(element, remplace));
+};
+
+function concatString(str1, str2) {
+	return (str1+str2);
+};
+
+function afficherCar(str, n) {
+	return (str.charAt(n-1));
+};
+
+function afficherNCar(str, n) {
+	return (str.substring(0,9));
+};
+
+function majusculeString(str) {
+	return (str.toUpperCase());
+};
+
+function SupprEspaceString(str) {
+	return (str.trim());
+};
+
+function IsString(str) {
+	 return (typeof str === 'string');
+};
+
+function AfficherExtensionString(str) {
+	
+};
+
+function NombreEspaceString(str) {
+	return (str.match(/[" "]/g).length);
+};
+
+function InverseString(str) {
+	return str.reverse();
+};
+
+function minusculeString(str) {
+	return str.toLowerCase();
+};
+
+function countDistinctCar(str) {
+
+};
+
+
+
+
+
+
+
+
+function CalculPuissance(n, e) {
+
+};
+
+function valeurAbsolue(n) {
+
+};
+
+function valeurAbsolueArray(array) {
+
+};
